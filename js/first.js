e775e42b068602eeb812a43238a2167df6a4d4a0
raddevus <1radeutsch1@gmail.com>
2025-03-17 16:00:39 -0400

    Added first successful export / import. Works great!

---
+++ js/first.js
@@ -0,0 +1,6 @@
+// first.js
+// to use as example for first function imported into main.js
+
+export function getDateTime(){
+	return new Date().toString();
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 // main.js
+import {getDateTime} from "./first.js"
 
 addEventListener("load", appmain);
 
 function appmain(){
 	console.log("I'm in here.");
+	document.querySelector("#date").textContent = getDateTime();
 }
